59855e7b3225f0a4259f7f354b06e9afad5d4603
CourageT1 <tcgwizhi@gmail.com>
2023-06-14 11:59:39 -0700

    fixing identation on task4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,5 +6,5 @@ def only_diff_elements(set_1, set_2):
             unique_elements.add(element)
     for element in set_2:
            if element not in set_1:
-            unique_elements.add(element)
+               unique_elements.add(element)
     return unique_elements
