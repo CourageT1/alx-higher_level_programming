1d2194ea6eb70efa5c6b0ca1fe795fc87d77aa1d
CourageT1 <tcgwizhi@gmail.com>
2023-06-26 12:47:38 -0700

    task0-5

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,10 @@ def safe_print_list(my_list=[], x=0):
         for item in my_list:
             if count == x:
                 break
-            print(item, end=" ")
+            print(item, end="")
             count += 1
-    except IndexError:
+    except (IndexError):
         pass
     finally:
         print()
-        return count
+        return (count)
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,13 +4,13 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for item in my_list:
             try:
-                print("{:d}".format(item), end=" ")
+                print("{:d}".format(item), end="")
                 count += 1
             except (ValueError, TypeError):
                 pass
             if count == x:
                 break
-    except (IndexError):
+    except IndexError:
         pass
     finally:
         print()
