8071dc9d3ac4d39591d754091d8c187381edcff0
CourageT1 <tcgwizhi@gmail.com>
2023-06-26 11:00:00 -0700

    task2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in my_list:
+            try:
+                print("{:d}".format(item), end=" ")
+                count += 1
+            except (ValueError, TypeError):
+                pass
+            if count == x:
+                break
+    except (IndexError):
+        pass
+    finally:
+        print()
+        return (count)
