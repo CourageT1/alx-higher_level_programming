0d9a0eede23096b6c027d70aaa0837e415cfaca0
CourageT1 <tcgwizhi@gmail.com>
2023-06-27 12:13:01 -0700

    mandatory

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define class square"""
+
+
 class Square:
     """Represent a square."""
     def __init__(self, size):
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a square with given size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a square with the given size.
+
+        Args:
+            size (int): The size of the square (default 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
